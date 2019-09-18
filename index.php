4f4b350beb8bf881f8411a583bc4f4ea0ccd41da
nyanchan <personal@nyanchangames.com>
2019-09-18 22:41:20 +0900

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+﻿<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title> Hello experiment </title>
+</head>
+<body >
+<?php
+date_default_timezone_set('Asia/Tokyo');
+echo("<h1>Hello Experiment! It's ".date("Y-m-d H:i:s")."!</h1>");
+echo("<p>実験頑張るんやで!</p>");
+?>
+</body>
+</html>
